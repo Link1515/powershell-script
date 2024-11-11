0711977d15a248b55f2fa6b03a7f471bd290e344
Link1515 <h26281660@gmail.com>
2024-11-11 18:02:36 +0800

    add init script

---
+++ init.ps1
@@ -0,0 +1,100 @@
+# 調整 ExecutionPolicy 等級到 RemoteSigned
+Set-ExecutionPolicy RemoteSigned -Force
+
+# 安裝 Chocolatey
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+# 安裝 windows terminal
+choco install microsoft-windows-terminal -y
+# 安裝 oh-my-posh
+choco install oh-my-posh -y
+$themeDownloadUrl = "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/blue-owl.omp.json"
+$themePath = "C:\Users\Public\Downloads\blue-owl.omp.json"
+Invoke-WebRequest -Uri $themeDownloadUrl -OutFile $destinationPath
+
+# 建立 $PROFILE 所需的資料夾
+New-Item -Path $PROFILE -Type File -Force
+
+# 設定 PowerShell 的 ProgressPreference, TLS 1.2 與 PSReadLine 快速鍵
+# https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables#progresspreference
+@'
+# 修正 PowerShell 關閉進度列提示
+# $ProgressPreference = 'SilentlyContinue'
+
+# 使用 TLS 1.2 進行網路安全連線
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# 設定按下 Ctrl+d 可以退出 PowerShell 執行環境
+Set-PSReadlineKeyHandler -Chord ctrl+d -Function ViExit
+
+# 設定按下 Ctrl+w 可以刪除一個單字
+Set-PSReadlineKeyHandler -Chord ctrl+w -Function BackwardDeleteWord
+
+# 設定按下 Ctrl+e 可以移動游標到最後面(End)
+Set-PSReadlineKeyHandler -Chord ctrl+e -Function EndOfLine
+
+# 設定按下 Ctrl+a 可以移動游標到最前面(Begin)
+Set-PSReadlineKeyHandler -Chord ctrl+a -Function BeginningOfLine
+
+function hosts { nvim c:\windows\system32\drivers\etc\hosts }
+
+# 移除內建的 curl 與 wget 命令別名
+If (Test-Path Alias:curl) {Remove-Item Alias:curl}
+If (Test-Path Alias:wget) {Remove-Item Alias:wget}
+
+# 使用 oh-my-posh
+oh-my-posh init pwsh --config $themePath | Invoke-Expression
+'@ | Out-File $PROFILE
+
+. $PROFILE
+
+# 安装常用字型
+choco install sourcecodepro -y
+
+# 安裝 git
+choco install git -y
+# Chocolatey 安裝 Git 後雖然有註冊 PATH 環境變數，但目前工作階段並沒有註冊
+$env:Path += ';C:\Program Files\Git\cmd'
+
+# 設定預設 Git Alias
+git config --global alias.ci   commit
+git config --global alias.cm   "commit --amend -C HEAD"
+git config --global alias.co   checkout
+git config --global alias.st   status
+git config --global alias.sts  "status -s"
+git config --global alias.br   branch
+git config --global alias.re   remote
+git config --global alias.di   diff
+git config --global alias.type "cat-file -t"
+git config --global alias.dump "cat-file -p"
+git config --global alias.lo   "log --oneline"
+git config --global alias.ls   "log --show-signature"
+git config --global alias.ll   "log --pretty=format:'%h %ad | %s%d [%Cgreen%an%Creset]' --graph --date=short"
+git config --global alias.lg   "log --graph --pretty=format:'%Cred%h%Creset %ad |%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset [%Cgreen%an%Creset]' --abbrev-commit --date=short"
+git config --global alias.alias "config --get-regexp ^alias\."
+git config --global alias.ignore "!gi() { curl -sL https://www.gitignore.io/api/\$@ ;}; gi"
+git config --global alias.iac  "!giac() { git init && git add . && git commit -m 'Initial commit' ;}; giac"
+git config --global alias.rc  "!grc() { git reset --hard && git clean -fdx ;}; read -p 'Do you want to run the <<< git reset --hard && git clean -fdx >>> command? (Y/N) ' answer && [[ $answer == [Yy] ]] && grc"
+
+# 直接設定 Git 預設 user.name 與 user.email
+git config --global user.name "Your Name"
+git config --global user.email "you@example.com"
+
+# 安裝常用軟體
+choco install wget neovim vscode docker-desktop dbeaver postman line -y
+
+# 安裝 wsl
+wsl --install
+
+# 安裝 nvm 與 node lts
+choco install nvm.portable -y
+refreshenv
+nvm install lts
+nvm use lts
+npm i -g pnpm
+
+# 安裝 PowerShell core
+choco install powershell-core -y
+
+# 安裝 brave 瀏覽器
+choco install brave --ignorechecksums -y
